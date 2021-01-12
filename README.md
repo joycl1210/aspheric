e6c14051573af44c58b1b531c01ec67601b85d80
mirequem <68602207+mirequem@users.noreply.github.com>
2021-01-12 14:19:41 -0500

    added beam diameter

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# aspheric
+# Aspheric
+
+This code helps understanding the raytracing of the plano-convex aspheric lenses, the motivation of the use of the paraxial approximation and how to choose the right conic constant for the design.
+
+## User parameters
+
+The user specifies in the code various parameters for the lens and the simulation.
+
+- `kappa`: conic constant 
+- `d`: diameter of the plano convex lens [mm]
+- `R`: radius of curvature of the lens [mm]. For a convex lens, the radius of curvature must be a negative value.
+- `n`: refractive index of the lens
+- `L`: propagation length behind the lens [mm]
+- `n_ray`: number of rays incident on the surface
+- `n_pt`: number of points that constitute the aspheric surface
+- `w`: beam diameter [mm]
+
+
+
+## Outputs graphs
+
+### Raytracing
+
+<img src="/Users/mireillequemener/Documents/GitHub/aspheric/raytracing.png" alt="raytracing" style="zoom:33%;" />
+
+### Ray fan plot
+
+
+
+## Examples
+
+### Paraxial approximation
+
--- aspheric_raytracing.py
+++ aspheric_raytracing.py
@@ -10,13 +10,17 @@ import math
 # L: propagation length behind the lens
 # n_ray: number of rays incident on the surface
 # m: number of points that constitute the aspheric surface
-kappa = -(1.5168**2)
+kappa = 0
 d = 60
 R = -50.23
 n = 1.5168
 L = 150
-n_ray = 15
-m = 100
+n_ray = 11
+n_pt = 100
+w = 50
+
+if (d < w):
+    exit('The beam diameter cannot be larger than the diameter of the lens')
 
 rmax = d/2
 f = -1/((1/R)*(n-1))
@@ -24,7 +28,7 @@ print("The focal length is:", f, "mm")
 
 # Calculate the surface of the lens and plot
 
-r_surf = np.arange(-rmax, rmax, rmax/m)
+r_surf = np.arange(-rmax, rmax, rmax/n_pt)
 z = []
 for i in range(len(r_surf)):
     z.append(r_surf[i]**2/(R*(1+math.sqrt(1-(1+kappa)*r_surf[i]**2/R**2))))
@@ -34,7 +38,7 @@ plt.plot(z, r_surf)
 # Raytracing
 
 # Incident rays tracing
-r_ray = np.arange(-rmax, rmax, 2*rmax/n_ray)
+r_ray = np.arange(-w/2, w/2, w/n_ray)
 z_ray = []
 for i in range(len(r_ray)):
     z_ray.append(r_ray[i]**2/(R*(1+math.sqrt(1-(1+kappa)*r_ray[i]**2/R**2))))
@@ -64,17 +68,19 @@ for i in range(len(r_ray)):
     y2 = [r_ray[i], L*np.tan(theta_2[i]) + r_ray[i]]
     plt.plot(x2, y2, color='red')
 plt.gca().set_aspect('equal', adjustable='box')
+plt.xlabel('Axial distance [mm]')
+plt.ylabel('Height [mm]')
 plt.show()
 
 # Zoom plot on the focal spot
-zrtrace = plt.figure(2)
-for i in range(len(r_ray)):
-    x2 = [z_ray[i], z_ray[i]+L]
-    y2 = [r_ray[i], L*np.tan(theta_2[i]) + r_ray[i]]
-    plt.plot(x2, y2, color='red')
-plt.xlim(f-0.1*f, f+0.1*f)
-plt.ylim(-0.2, 0.2)
-plt.show()
+# zrtrace = plt.figure(2)
+# for i in range(len(r_ray)):
+    # x2 = [z_ray[i], z_ray[i]+L]
+    # y2 = [r_ray[i], L*np.tan(theta_2[i]) + r_ray[i]]
+    # plt.plot(x2, y2, color='red')
+# plt.xlim(f-0.1*f, f+0.1*f)
+# plt.ylim(-0.2, 0.2)
+# plt.show()
 
 # Ray fan plot
 y_f = []
@@ -88,4 +94,6 @@ axs.spines['left'].set_position('zero')
 axs.spines['right'].set_color('none')
 axs.spines['bottom'].set_position('zero')
 axs.spines['top'].set_color('none')
+plt.xlabel('y')
+plt.ylabel("y'")
 plt.show()
